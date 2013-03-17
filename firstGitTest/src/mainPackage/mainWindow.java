0d96c59c39cf0b2cb1e4865cdb47b4007d03042d
Magnus Tornquist <magnusvmt@gmail.com>
2013-03-18 00:11:43 +0100

    removed the old main method and implemented a new one for testing display settings.

---
+++ firstGitTest/src/mainPackage/mainWindow.java
@@ -0,0 +1,43 @@
+package mainPackage;
+
+import graphicsHandling.Screen;
+
+import java.awt.Color;
+import java.awt.DisplayMode;
+import java.awt.Font;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+
+public class mainWindow extends JFrame{
+	int resWidth = 1680;
+	int resHeigth = 1050;
+	
+	public static void main(String[] args){
+
+		DisplayMode dm = new DisplayMode(1680,1050,16,DisplayMode.REFRESH_RATE_UNKNOWN);
+		mainWindow m = new mainWindow();
+		m.run(dm);
+	}
+	
+	public void run(DisplayMode dm){
+		setBackground(Color.PINK);
+		setForeground(Color.WHITE);
+		setFont(new Font("Arial", Font.BOLD, 24));
+		Screen s = new Screen();
+		try{
+			s.setFullScreen(dm, this);
+			try{
+				Thread.sleep(5000);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}finally{
+			s.restoreScreen();
+		}
+	}
+	
+	public void paint(Graphics g){
+		g.drawString("This is gonna be awesome", resWidth/2, resHeigth/2);
+	}
+}
